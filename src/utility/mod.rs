a72c34f8deea52bdd981f235d0549eec8a88fdb3
Pericles Telemachou <pericles@outlook.com.au>
2025-02-13 15:40:13 +1100

    Add `get_cwd` utility

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 pub const KILOBYTE: usize = 1024;
 pub const MEGABYTE: usize = KILOBYTE * 1024;
 pub const GIGABYTE: usize = MEGABYTE * 1024;
@@ -24,4 +26,16 @@ pub fn get_shorthand_memory_limit(amount: i64) -> String {
         }
     }
     return format!("{}{}{}", sign, amount_abs / mult, unit)
+}
+
+pub fn get_cwd () -> PathBuf {
+    let wd_or_err = std::env::current_dir();
+    match wd_or_err {
+        Ok(wd) => {
+            return wd;
+        }
+        Err(e) => {
+            panic!("error getting cwd: {}", e);
+        }
+    }
 }
