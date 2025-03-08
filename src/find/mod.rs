eac57bc30cb18cfd10f0e96807fb21b3ea550398
Pericles Telemachou <pericles@outlook.com.au>
2025-03-08 20:36:20 +1100

    Update `find()` to pass filter/"sorted" to thread function and sort items IF requested

--- src/find/mod.rs
+++ src/find/mod.rs
@@ -1,13 +1,28 @@
 use std::{collections::HashSet, io::Error};
 
-use crate::{utility::thread_from_root, walk::walk_search_until_limit};
+use crate::{utility::thread_from_root_new, walk::walk_search_until_limit};
 
-pub fn find(target_substring: String, target_path: std::path::PathBuf, num_threads: usize, thread_add_dir_limit: usize, show_hidden: bool) -> Result<Vec<String>, Error> {
+// t: 0 -> dir, 1 -> dir, 2 -> symlink, 3 -> other
+#[derive(Debug, Clone)]
+pub struct FoundFile {
+    pub p: String,
+    pub is_sym: bool,
+    pub is_hidden: bool,
+}
+
+pub fn find(target_substring: String, target_path: std::path::PathBuf, num_threads: usize, thread_add_dir_limit: usize, show_hidden: bool, sorted: bool) -> Result<Vec<String>, Error> {
     if num_threads <= 1 {
         return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Provided 0 or 1 number of threads, must provide '-t' argument > 1")))
     }
-
-    let maybe_curr_scan: std::io::Result<Vec<String>> = thread_from_root(
+    
+    let mut maybe_filter: Option<fn(a: &FoundFile) -> bool> = None;
+    if !show_hidden {
+        maybe_filter = Some(|a: &FoundFile| {
+            return !a.is_hidden;
+        })
+    }
+    
+    let maybe_curr_scan: std::io::Result<Vec<FoundFile>> = thread_from_root_new(
         target_path, 
         HashSet::new(),
         &target_substring, 
@@ -16,18 +31,31 @@ pub fn find(target_substring: String, target_path: std::path::PathBuf, num_threa
         None, 
         Some(walk_search_until_limit), 
         |a, b| {
-            return a.cmp(b);
+            return a.p.cmp(&b.p);
         },
-        show_hidden
+        maybe_filter,
+        sorted,
+        |a| {
+            a.p
+        }
     );
+    
     if maybe_curr_scan.is_err() {
         return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to do MT find: {:?}", maybe_curr_scan.err())))
     }
+    
+    if !sorted {
+        return Ok(Vec::new())
+    }
+
     let mut curr_scan = maybe_curr_scan.unwrap();
-
     curr_scan.sort_by(|a, b| {
-        return a.cmp(b);
+        return a.p.cmp(&b.p);
     });
+    
+    let ret = curr_scan.into_iter().map(|s|{
+        return s.p
+    }).collect();
 
-    Ok(curr_scan)
+    return Ok(ret);
 }
